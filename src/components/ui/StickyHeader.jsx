7396cb622244b60402c974556aa2b63d32608b20
meruff <mathew.ruff@gmail.com>
2025-03-04 08:20:29 -0700

    Make header transition like toolbar.

--- src/components/ui/StickyHeader.jsx
+++ src/components/ui/StickyHeader.jsx
@@ -6,7 +6,7 @@ export default function Info({ data, isScrolled }) {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 right-0 z-50 bg-gray-100 dark:bg-gray-800 shadow-lg transition-transform ease-in-out duration-300 ${
+        className={`fixed top-0 left-0 right-0 z-50 bg-gray-100 dark:bg-gray-800 shadow-lg transition-transform duration-300 ${
           isScrolled ? "transform translate-y-0" : "transform -translate-y-full"
         }`}
       >
