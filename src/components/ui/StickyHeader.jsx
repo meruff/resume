03de65d55b725b33934144cc8c198c9ddb17ab26
meruff <mathew.ruff@gmail.com>
2025-03-04 07:34:50 -0700

    update StickyHeader background colors to match theme.

--- src/components/ui/StickyHeader.jsx
+++ src/components/ui/StickyHeader.jsx
@@ -6,7 +6,7 @@ export default function Info({ data, isScrolled }) {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 right-0 z-50 bg-white dark:bg-gray-900 bg-gradient-to-br from-gray-100 via-custom-light/10 to-gray-200 dark:from-gray-800 dark:via-custom-dark/10 dark:to-gray-900 shadow-lg transition-transform ease-in-out duration-300 ${
+        className={`fixed top-0 left-0 right-0 z-50 bg-gray-100 dark:bg-gray-800 shadow-lg transition-transform ease-in-out duration-300 ${
           isScrolled ? "transform translate-y-0" : "transform -translate-y-full"
         }`}
       >
